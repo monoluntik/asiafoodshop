235b67a893c24b8da16a93110c3bf86751155fdf
Your Name <you@example.com>
2021-11-10 14:15:50 +0600

    first view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from main.models import Category
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'index.html'
+    context_object_name = 'categories'

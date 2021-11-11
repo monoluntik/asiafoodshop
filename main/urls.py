a4c585582725b2f6d82e4d9741af6a564463bf4e
monoluntik <narbkv07@gmail.com>
2021-11-11 12:48:25 +0600

    search

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='create'),
     path('update/<int:product_id>/', ProductUpdateView.as_view(), name='update'),
     path('delete/<int:product_id>/', ProductDeleteView.as_view(), name='delete'),
-    path('product/detail/<int:product_id>/', DetailListView.as_view(), name='product-detail')
+    path('product/detail/<int:product_id>/', DetailListView.as_view(), name='product-detail'),
+    path('search', SearchListView.as_view(), name='search'),
 ]

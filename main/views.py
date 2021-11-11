5b7039dfa2fc97f83e96b18065e670a7ee900d4f
Your Name <you@example.com>
2021-11-11 10:21:35 +0600

    Home page

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.shortcuts import redirect
 
-
 from .models import *
 from .forms import CreateProductForm, UpdateProductForm
+
 
 class CategoryListView(ListView):
     model = Category
@@ -16,7 +16,6 @@ class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
     context_object_name = 'products'
-
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -41,6 +40,7 @@ class ProductCreateView(CreateView):
         context['product_form'] = self.get_form(self.get_form_class())
         return context
 
+
 class ProductUpdateView(UpdateView):
     model = Product
     template_name = 'create.html'
@@ -51,3 +51,4 @@ class ProductUpdateView(UpdateView):
         context = super().get_context_data(**kwargs)
         context['product_form'] = self.get_form(self.get_form_class())
         return context
+

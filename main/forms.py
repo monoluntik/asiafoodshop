eeb7775aac6964dd0331edddfe40ffdea0407134
Your Name <you@example.com>
2021-11-13 18:41:44 +0600

    Comments

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.forms import widgets
 
-from .models import Product
+from .models import *
 
 
 class CreateProductForm(forms.ModelForm):
@@ -12,3 +13,10 @@ class UpdateProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+
+
